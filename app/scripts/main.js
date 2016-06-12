1a8420185b00511be931e1365fa01aa1a6e48a7d
Anabelle Handdoek <ana@gathacol.net>
2016-06-12 17:24:13 -0500

    minor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,40 @@
 // 1. This is our program
 var program = {
-								"12:00": "Andrea Fraser (US)",
-								"12:35": "Christian Bök (CA) Kenneth Goldsmith (US)",
-								"13:20": "Mark Leckey (UK)",
-								"14:10": "Aram Bartholl (DE)",
-								"15:00": "Olia Lialina (RU)",
-								"15:50": "Etoy (EU)",
-								"16:20": "Minerva Cuevas (MX)",
-								"16:45": "Sibylle Peters (DE)",
-								"17:15": "Fred Wilson (US)",
-								"18:15": "Paola Pivi (IT)",
-								"19:15": "Andres Burbano (CO)",
-								"19:40": "David Claerbout (BE)",
-								"21:05": "Adrián Villar Rojas (AR)",
-								"22:00": "Barbara Visser (NL)",
-								"22:25": "Jérôme Bel (FR)",
-								"22:50": "Fay Nicolson (UK)"
+								'12:00': 'Andrea Fraser (US)',
+								'12:35': 'Christian Bök (CA) Kenneth Goldsmith (US)',
+								'13:20': 'Mark Leckey (UK)',
+								'14:10': 'Aram Bartholl (DE)',
+								'15:00': 'Olia Lialina (RU)',
+								'15:50': 'Etoy (EU)',
+								'16:20': 'Minerva Cuevas (MX)',
+								'16:45': 'Sibylle Peters (DE)',
+								'17:15': 'Fred Wilson (US)',
+								'18:15': 'Paola Pivi (IT)',
+								'19:15': 'Andres Burbano (CO)',
+								'19:40': 'David Claerbout (BE)',
+								'21:05': 'Adrián Villar Rojas (AR)',
+								'22:00': 'Barbara Visser (NL)',
+								'22:25': 'Jérôme Bel (FR)',
+								'22:50': 'Fay Nicolson (UK)'
 							};
 
 var videos = [
-								"M7lc1UVf-VE",
-								"M7lc1UVf-VE",
-								"M7lc1UVf-VE",
-								"M7lc1UVf-VE",
-								"M7lc1UVf-VE",
-								"X7JQu06ylFM",
-								"M7lc1UVf-VE",
-								"X7JQu06ylFM",
-								"M7lc1UVf-VE",
-								"X7JQu06ylFM",
-								"M7lc1UVf-VE",
-								"X7JQu06ylFM",
-								"M7lc1UVf-VE",
-								"X7JQu06ylFM",
-								"M7lc1UVf-VE",
-								"X7JQu06ylFM"
+								'M7lc1UVf-VE',
+								'M7lc1UVf-VE',
+								'M7lc1UVf-VE',
+								'M7lc1UVf-VE',
+								'M7lc1UVf-VE',
+								'X7JQu06ylFM',
+								'M7lc1UVf-VE',
+								'X7JQu06ylFM',
+								'M7lc1UVf-VE',
+								'X7JQu06ylFM',
+								'M7lc1UVf-VE',
+								'X7JQu06ylFM',
+								'M7lc1UVf-VE',
+								'X7JQu06ylFM',
+								'M7lc1UVf-VE',
+								'X7JQu06ylFM'
 ];
 
 var starttime_global = 0;
@@ -49,10 +49,10 @@ function initPlayer(){
 }
 
 function buildProgram() {
-	var currenttime = Date.parse("now").getTime()/1000;	
+	var currenttime = Date.parse('now').getTime()/1000;	
 	for (var key in program) {
 		 if (program.hasOwnProperty(key)) {
-		    $('.program dl').append( "<dt>" + key + "</dt><dd>" + program[key] + "</dd>" );
+		    $('.program dl').append( '<dt>' + key + '</dt><dd>' + program[key] + '</dd>' );
 		 }
 	}
 }
@@ -62,7 +62,7 @@ var currentvideo = -1;
 function nowPlaying() {
 	clearTimeouts();
 	var count = 0;
-	var currenttime = Date.parse("now").getTime()/1000;
+	var currenttime = Date.parse('now').getTime()/1000;
 	for (var key in program) {
 		 if (program.hasOwnProperty(key)) {
 				var starttime = Date.parse( key ).getTime()/1000;
@@ -73,7 +73,7 @@ function nowPlaying() {
 				}else{
 					var remaining = starttime - currenttime;
 					timeOuts[ key ] = setTimeout( function() {initPlayer()}, remaining*1000 );
-					console.log( "Set timeout: timeOuts[" +key+"] starting in: " + remaining );
+					console.log( 'Set timeout: timeOuts[' +key+'] starting in: ' + remaining );
 				}
 		    count = count + 1;
 		 }
@@ -94,9 +94,9 @@ function clearTimeouts() {
 
 var player = {};
 function buildPlayer() {
-	$("#player-container").show();
+	$('#player-container').show();
 	var play = nowPlaying();
-	console.log( "play: " + play );
+	console.log( 'play: ' + play );
 	player = new YT.Player('player', {
 		height: '100%',
 		width: '100%',
@@ -118,13 +118,13 @@ function buildPlayer() {
 }
 
 function syncPlayer( event ) {
-	console.log( "global: " + starttime_global);
+	console.log( 'global: ' + starttime_global);
 
-	var currenttime = Date.parse("now").getTime()/1000;
+	var currenttime = Date.parse('now').getTime()/1000;
   var duracion = event.target.getDuration();
 
   var timeInto = currenttime - starttime_global;
-  console.log( "timeInto: " + timeInto );
+  console.log( 'timeInto: ' + timeInto );
 	
 	if( timeInto < duracion ){
 		player.seekTo( timeInto );
@@ -135,13 +135,13 @@ function syncPlayer( event ) {
 }
 
 function talkEnded() {
-	$("#player-container").hide();
+	$('#player-container').hide();
 }
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
+tag.src = 'https://www.youtube.com/iframe_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
@@ -177,11 +177,11 @@ function onPlayerStateChange(event) {
 }
 
 function tomorrowRestart(){
-	var currenttime = Date.parse("now").getTime()/1000;	
-	var tomorrow = Date.parse("tomorrow").getTime()/1000;
+	var currenttime = Date.parse('now').getTime()/1000;	
+	var tomorrow = Date.parse('tomorrow').getTime()/1000;
 
 	var untilTomorrow = tomorrow - currenttime;
-	timeOuts[ "tomorrow" ] = setTimeout( function() {initPlayer()}, untilTomorrow*1000 );
+	timeOuts[ 'tomorrow' ] = setTimeout( function() {initPlayer()}, untilTomorrow*1000 );
 
-	console.log( "The Artist Talk Fest Starts Again in: " + untilTomorrow );
+	console.log( 'The Artist Talk Fest Starts Again in: ' + untilTomorrow );
 }
